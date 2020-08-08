de90da51f7a3cb12fe7e61e1380706786e80950b
Somto M.Ugeh <smugeh@gmail.com>
2020-08-08 23:06:23 +0100

    updated build

--- dist/useMono.d.ts
+++ dist/useMono.d.ts
@@ -4,4 +4,4 @@ import { MonoProps, MonoConfig } from './types';
  * @param config takes in configuration for mono
  * @returns handleMono function
  */
-export default function useKorapay(monoConfig: MonoConfig): ({ onSuccess, onClose }: MonoProps) => void;
+export default function useMono({ public_key, }: MonoConfig): ({ onSuccess, onClose }: MonoProps) => void;
